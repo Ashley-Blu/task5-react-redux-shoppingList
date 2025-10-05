65310a4f90311db953d790ed59650458f9f82284
Ashley-Blu <matsekolengashley28@hmail.com>
2025-10-05 22:32:30 +0200

    Added 2 functioning button components

---
+++ src/components/SigninButton.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+export const SigninButton: React.FC = () =>{
+    return(
+        <>
+            <div className="mt-15">
+                <button className="w-[358px] h-[51px] bg-green-600 rounded">Sign In</button>
+            </div>
+        </>
+    )
+}
---
+++ src/components/SignupButton.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+export const SignupButton: React.FC = () =>{
+    return(
+        <>
+            <div className="mt-5">
+                <button className="w-[358px] h-[51px] border-3 rounded">Sign Up</button>
+            </div>
+        </>
+    )
+}
